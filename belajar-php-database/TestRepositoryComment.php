9b99f539f238972cbcc61e39f35c2421a78ececb
Abdullah Muchsin <90706883+AbdullahMuchsin@users.noreply.github.com>
2024-09-05 12:44:25 +0700

    Inisialisai Commit

--- belajar-php-database/TestRepositoryComment.php
+++ belajar-php-database/TestRepositoryComment.php
@@ -9,6 +9,6 @@ require_once __DIR__ . "/Repository/CommentRepository.php";
 
 $comment = new CommentRepositoryImpl(getConnection());
 
-$data = $comment->getDataById(2);
+$data = $comment->getDataAll();
 
 var_dump($data);
